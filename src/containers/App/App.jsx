a12115d96992889fbaac9e572ead1abf07741ac5
sangeethksin <sangeeth.ks@mobiotics.com>
2018-04-02 10:46:42 +0530

    Code cleanup

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './App.scss';
+
+class App extends Component {
+  constructor () {
+    super(props);
+  } // constructor
+
+  render() {
+    return (
+
+    );
+  } // render
+}
+
+export default App;
