7746b8237f00072f1d577ba44ec0e4432a32a49c
Sangeethks <sangeethks.in@gmail.com>
2018-04-17 09:31:07 +0530

    Created store

---
+++ src/Store/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  counter: 0
+}
+
+// Initializing reducer with initial state
+const reducer = (state = initialState, action) => {
+
+}
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+// To create redux store
+import { createStore } from 'redux';
+
 // import registerServiceWorker from './registerServiceWorker';
+
+// Defining reducer to initialize the store
+import reducer from './Store/reducer';
+// Creating the Store
+const Store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
